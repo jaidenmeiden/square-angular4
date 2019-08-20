405ca213261a15d31b7e1fefdd1a44b29a3be0ff
Jaiden Meiden <jaidenmeiden@gmail.com>
2019-08-19 23:38:24 -0500

    Ajustes al proyecto con directivas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import {ResaltarDirective} from "./directives/resaltar.directive";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResaltarDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
