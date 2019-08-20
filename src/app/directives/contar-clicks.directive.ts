e8240571d13b187c65bb89207d474c6323d4994a
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-08-20 11:58:57 -0500

    Agregar Host Binding y combinado con Host Listeners

--- src/app/directives/contar-clicks.directive.ts
+++ src/app/directives/contar-clicks.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, HostListener} from "@angular/core";
+import {Directive, HostBinding, HostListener} from "@angular/core";
 //Se incluye a traves de 'Directive' el selector de la directiva personalizada y va a ser una directiva de atributo
 @Directive({
   selector: 'a[contar-clicks]'//La 'a' al principio determina que solo aplica a hipervinculos
@@ -6,8 +6,11 @@ import {Directive, HostListener} from "@angular/core";
 //Se exporta la clase para poderla usuar fuera del archivo
 export class ContarClicksDirective {
   clickN = 0;//Lleva el conteo de los clicks
+  //El HostListener permite modificar directamente un elemento. Recibe: 1. Atributo del elemento HTML
+  @HostBinding('style.opacity') opacity: number = 0.1;
   //El HostListener permite escuchar los eventos. Recibe: 1. Evento, 2. Un arreglo con el target
   @HostListener('click', ['$event.target']) onClick(btn) {//El evento se dispara en un onClick
-    console.log('a', btn, "Número de clicks: ", this.clickN++);
+    console.log('a', btn, "Número de clicks: ", this.clickN++);//Incrementa contador
+    this.opacity += 0.1;
   }
 }
