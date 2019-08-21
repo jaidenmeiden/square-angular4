376be55f0de2f130b86b608651eecb980e01ad82
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-08-21 07:01:30 -0500

    Creando una vista de detalle para el proyecto

--- src/app/components/detalle/detalle.component.ts
+++ src/app/components/detalle/detalle.component.ts
@@ -7,7 +7,17 @@ import {ActivatedRoute} from '@angular/router';
   styleUrls: ['./detalle.component.css']
 })
 export class DetalleComponent implements OnInit {
-
+  lugares:any = [
+    {id: 1 ,plan: 'gratuito', cercania: 1, distancia: 1, active: true, nombre:'Florería la Gardenia'},
+    {id: 2 ,plan: 'gratuito', cercania: 1, distancia: 1.8, active: true, nombre:'Donas la pasadita'},
+    {id: 3 ,plan: 'gratuito', cercania: 2, distancia: 5, active: true, nombre:'Veterinaria Huellitas Felices'},
+    {id: 4 ,plan: 'gratuito', cercania: 2, distancia: 10, active: false, nombre:'Sushi Suhiroll'},
+    {id: 5 ,plan: 'gratuito', cercania: 3, distancia: 35, active: true, nombre:'Hotel la Gracia'},
+    {id: 6 ,plan: 'gratuito', cercania: 3, distancia: 120, active: false, nombre:'Zapatería el Clavo'},
+    {id: 7 ,plan: 'pagado', cercania: 1, distancia: 3, active: true, nombre:'Panadería de Chía'},
+    {id: 8 ,plan: 'pagado', cercania: 3, distancia: 90, active: true, nombre:'Bizcochería'}
+  ];
+  id = null;
   constructor(
     private route: ActivatedRoute
   ) {
@@ -15,9 +25,14 @@ export class DetalleComponent implements OnInit {
     console.log(this.route.snapshot.queryParams);
     console.log(this.route.snapshot.queryParams['action']);
     console.log(this.route.snapshot.queryParams['referer']);
+    this.id = this.route.snapshot.params['id'];
+    console.log(this.buscarLugar());
 }
 
   ngOnInit() {
   }
 
+  buscarLugar() {
+    return this.lugares.filter((lugar) => { return lugar.id = this.id})[0] || null;
+  }
 }
