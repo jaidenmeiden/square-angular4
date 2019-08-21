31a61e5781f2591b5b13f7c5bb17cb6cc73bbc93
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-08-21 06:46:30 -0500

    Parámetros en Rutas

--- src/app/components/lugares/lugares.component.ts
+++ src/app/components/lugares/lugares.component.ts
@@ -9,14 +9,14 @@ export class LugaresComponent implements OnInit {
   title = 'Platzi Square';
 
   lugares:any = [
-    {plan: 'gratuito', cercania: 1, distancia: 1, active: true, nombre:'Florería la Gardenia'},
-    {plan: 'gratuito', cercania: 1, distancia: 1.8, active: true, nombre:'Donas la pasadita'},
-    {plan: 'gratuito', cercania: 2, distancia: 5, active: true, nombre:'Veterinaria Huellitas Felices'},
-    {plan: 'gratuito', cercania: 2, distancia: 10, active: false, nombre:'Sushi Suhiroll'},
-    {plan: 'gratuito', cercania: 3, distancia: 35, active: true, nombre:'Hotel la Gracia'},
-    {plan: 'gratuito', cercania: 3, distancia: 120, active: false, nombre:'Zapatería el Clavo'},
-    {plan: 'pagado', cercania: 1, distancia: 3, active: true, nombre:'Panadería de Chía'},
-    {plan: 'pagado', cercania: 3, distancia: 90, active: true, nombre:'Bizcochería'}
+    {id: 1 ,plan: 'gratuito', cercania: 1, distancia: 1, active: true, nombre:'Florería la Gardenia'},
+    {id: 2 ,plan: 'gratuito', cercania: 1, distancia: 1.8, active: true, nombre:'Donas la pasadita'},
+    {id: 3 ,plan: 'gratuito', cercania: 2, distancia: 5, active: true, nombre:'Veterinaria Huellitas Felices'},
+    {id: 4 ,plan: 'gratuito', cercania: 2, distancia: 10, active: false, nombre:'Sushi Suhiroll'},
+    {id: 5 ,plan: 'gratuito', cercania: 3, distancia: 35, active: true, nombre:'Hotel la Gracia'},
+    {id: 6 ,plan: 'gratuito', cercania: 3, distancia: 120, active: false, nombre:'Zapatería el Clavo'},
+    {id: 7 ,plan: 'pagado', cercania: 1, distancia: 3, active: true, nombre:'Panadería de Chía'},
+    {id: 8 ,plan: 'pagado', cercania: 3, distancia: 90, active: true, nombre:'Bizcochería'}
   ];
   lat:number = 4.6560663;//(Google Maps)
   lng:number = -74.0595918;//(Google Maps)
