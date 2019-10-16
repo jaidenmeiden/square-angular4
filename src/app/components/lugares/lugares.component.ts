5d96b5864cf3ed62909a3bc23fd65a680679f8f6
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-10-16 13:45:51 -0500

    Implementar animacion destacados

--- src/app/components/lugares/lugares.component.ts
+++ src/app/components/lugares/lugares.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 // Servicios
 import {LugaresService} from "../../services/lugares.service";
 //Importar datos con firebase
@@ -8,7 +8,19 @@ import {Observable} from 'rxjs/internal/Observable';
 @Component({
   selector: 'app-lugares',
   templateUrl: './lugares.component.html',
-  styleUrls: ['./lugares.component.css']
+  styleUrls: ['./lugares.component.css'],
+  animations: [
+    trigger('contenedorAnimado', [
+      state('inicial', style({
+        opacity: 0
+      })),
+      state('final', style({
+        opacity: 1
+      })),
+      transition('inicial => final', animate(2000)),
+      transition('final => inicial', animate(1000))
+    ])
+  ]
 })
 export class LugaresComponent implements OnInit {
 
@@ -16,15 +28,20 @@ export class LugaresComponent implements OnInit {
   public lat:number = 4.854492;//(Google Maps)
   public lng:number = -74.053045;//(Google Maps)
   private lugares: Observable<any[]>;
+  state = 'inicial';
 
   constructor(
     private _lugaresServices: LugaresService
   ) {
     console.log("Hola Sockects...");
-    this.lugares = this._lugaresServices.getLugaresSockets();
+    this.iniciarCarga();
+    this.state = (this.state === 'final') ? 'inicial' : 'final';
   }
 
   ngOnInit() {
   }
 
+  iniciarCarga() {
+    this.lugares = this._lugaresServices.getLugaresSockets();
+  }
 }
